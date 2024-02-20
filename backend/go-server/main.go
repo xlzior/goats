763158be1dfe3fa131ef1df251d7a2e7719c7fa6
Sebastian Lee <leehur_sebastian@hotmail.com>
2024-02-20 16:57:20 +0800

    [SL] Handle CORS

--- backend/go-server/main.go
+++ backend/go-server/main.go
@@ -4,15 +4,23 @@ import (
 	"fmt"
 	"net/http"
 	"github.com/xlzior/goats/backend/go-server/handler"
+	"github.com/gorilla/handlers"
 )
 
 func main() {
-	
+
 	// Define the endpoint handler functions
 	http.HandleFunc("/parse", handler.ParseHandler)
 
+  // CORS middleware configuration
+  corsHandler := handlers.CORS(
+		handlers.AllowedOrigins([]string{"*"}),
+		handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}),
+		handlers.AllowedHeaders([]string{"Content-Type", "Authorization"}),
+	)
+
 	// Start the server on port 8080
 	fmt.Println("Go server listening on port 8080...")
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", corsHandler(http.DefaultServeMux))
 
 }
