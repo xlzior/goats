83efa82e4f6b2a5a4d43ccf68e5dd6f3d001ff67
xlzior <wenjun.lye@gmail.com>
2024-03-11 17:07:47 +0800

    [WJ] Format test programs

--- go-slang/__tests__/runner.test.ts
+++ go-slang/__tests__/runner.test.ts
@@ -9,11 +9,21 @@ beforeEach(() => {
 describe("Golang runner for evaluating binary expressions", () => {
   const singleLinebinaryExprTestCases = [
     {
-      program: 'package main\n\nimport "fmt"\n\nfunc main() {\n\t1 + 2\n}',
+      program: `
+      package main
+      import "fmt"
+      func main() {
+        1 + 2
+      }`,
       expected: 3,
     },
     {
-      program: 'package main\n\nimport "fmt"\n\nfunc main() {\n\t10 * 2 / 5\n}',
+      program: `
+      package main
+      import "fmt"
+      func main() {
+        10 * 2 / 5
+      }`,
       expected: 4.0,
     },
     // Add more test cases here
@@ -27,11 +37,17 @@ describe("Golang runner for evaluating binary expressions", () => {
     }
   );
 
-  test('evaluate program with multiple lines of binary expr should return the result of the last evaluated expr', async () => {
-    const program = 'package main\n\nimport "fmt"\n\nfunc main() {\n\t10 * 2 / 5\n33 - 13\n10 * 7 - 5\n}'
-    const actual = await golangRunner.execute(program)
-    const expected = 65
+  test("evaluate program with multiple lines of binary expr should return the result of the last evaluated expr", async () => {
+    const program = `
+    package main
+    import "fmt"
+    func main() {
+      10 * 2 / 5
+      33 - 13
+      10 * 7 - 5
+    }`;
+    const actual = await golangRunner.execute(program);
+    const expected = 65;
     expect(actual.value).toEqual(expected);
-  })
-
+  });
 });
