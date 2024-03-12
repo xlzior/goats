b3b439d417ef57efe11142063b1f1df385659953
xlzior <wenjun.lye@gmail.com>
2024-03-12 15:43:36 +0800

    [WJ] Add Token enum

--- go-slang/src/types/ast.ts
+++ go-slang/src/types/ast.ts
@@ -42,6 +42,113 @@ export enum Op {
   MINUS = "-",
 }
 
+export enum Token {
+  // Special tokens
+  ILLEGAL,
+  EOF,
+  COMMENT,
+
+  // Identifiers and basic type literals
+  // (these tokens stand for classes of literals)
+  IDENT, // main
+  INT, // 12345
+  FLOAT, // 123.45
+  IMAG, // 123.45i
+  CHAR, // 'a'
+  STRING, // "abc"
+
+  // Keywords
+  BREAK,
+  CASE,
+  CHAN,
+  CONST,
+  CONTINUE,
+
+  DEFAULT,
+  DEFER,
+  ELSE,
+  FALLTHROUGH,
+  FOR,
+
+  FUNC,
+  GO,
+  GOTO,
+  IF,
+  IMPORT,
+
+  INTERFACE,
+  MAP,
+  PACKAGE,
+  RANGE,
+  RETURN,
+
+  SELECT,
+  STRUCT,
+  SWITCH,
+  TYPE,
+  VAR,
+
+  // additional tokens, handled in an ad-hoc manner
+  TILDE,
+
+  // Operators and delimiters
+  ADD = "+",
+  SUB = "-",
+  MUL = "*",
+  QUO = "/",
+  REM = "%",
+
+  AND = "&",
+  OR = "|",
+  XOR = "^",
+  SHL = "<<",
+  SHR = ">>",
+  AND_NOT = "&^",
+
+  ADD_ASSIGN = "+=",
+  SUB_ASSIGN = "-=",
+  MUL_ASSIGN = "*=",
+  QUO_ASSIGN = "/=",
+  REM_ASSIGN = "%=",
+
+  AND_ASSIGN = "&=",
+  OR_ASSIGN = "|=",
+  XOR_ASSIGN = "^=",
+  SHL_ASSIGN = "<<=",
+  SHR_ASSIGN = ">>=",
+  AND_NOT_ASSIGN = "&^=",
+
+  LAND = "&&",
+  LOR = "||",
+  ARROW = "<-",
+  INC = "++",
+  DEC = "--",
+
+  EQL = "==",
+  LSS = "<",
+  GTR = ">",
+  ASSIGN = "=",
+  NOT = "!",
+
+  NEQ = "!=",
+  LEQ = "<=",
+  GEQ = ">=",
+  DEFINE = ":=",
+  ELLIPSIS = "...",
+
+  LPAREN = "(",
+  LBRACK = "[",
+  LBRACE = "{",
+  COMMA = ",",
+  PERIOD = ".",
+
+  RPAREN = ")",
+  RBRACK = "]",
+  RBRACE = "}",
+  SEMICOLON = ";",
+  COLON = ":",
+}
+
 // ========================
 // FILE - ROOT AST NODE
 // ========================
@@ -134,7 +241,7 @@ export interface AssignStmt extends Stmt {
   _type: NodeType.ASSIGN_STMT;
   Lhs: Ident[];
   Rhs: Expr[];
-  Tok: string; // TODO: add enum to represent tokens
+  Tok: Token;
 }
 
 export interface BlockStmt extends Stmt {
