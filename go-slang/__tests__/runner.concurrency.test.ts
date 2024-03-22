af7df19b9783ce94d204ee541e15e2f37a13e98d
xlzior <wenjun.lye@gmail.com>
2024-03-22 16:33:19 +0800

    [WJ] Implement tests for goroutines and interleaving
    Tests are non-deterministic if the sleeping time is too short

---
+++ go-slang/__tests__/runner.concurrency.test.ts
@@ -0,0 +1,68 @@
+import { GolangRunner } from "../src";
+
+let golangRunner: GolangRunner;
+
+const println = jest.fn();
+beforeEach(() => {
+  println.mockClear();
+  golangRunner = new GolangRunner({ Println: { arity: 1, apply: println } });
+});
+
+describe("Golang runner for evaluating concurrent programs", () => {
+  test("evaluate program with sleep", async () => {
+    const program = `
+    package main
+  
+    func main() {
+      Println(1)
+      Sleep(100)
+      Println(2)
+      Sleep(100)
+      Println(3)
+      Sleep(100)
+      Println(4)
+      Sleep(100)
+      Println(5)
+    }`;
+    const { value } = await golangRunner.execute(program);
+    const expected = undefined;
+    expect(value).toEqual(expected);
+
+    expect(println).toHaveBeenNthCalledWith(1, 1);
+    expect(println).toHaveBeenNthCalledWith(2, 2);
+    expect(println).toHaveBeenNthCalledWith(3, 3);
+    expect(println).toHaveBeenNthCalledWith(4, 4);
+    expect(println).toHaveBeenNthCalledWith(5, 5);
+  });
+
+  test("evaluate program with goroutines and interleaved execution", async () => {
+    const program = `
+    package main
+  
+    func thread(n int) {
+      x := 0
+      for x < 3 {
+        Println(100 * n + x)
+        x++
+        Sleep(200)
+      }
+    }
+    
+    func main() {
+      go thread(1)
+      Sleep(100)
+      go thread(2)
+      Sleep(700)
+    }`;
+    const { value } = await golangRunner.execute(program);
+    const expected = undefined;
+    expect(value).toEqual(expected);
+
+    expect(println).toHaveBeenNthCalledWith(1, 100);
+    expect(println).toHaveBeenNthCalledWith(2, 200);
+    expect(println).toHaveBeenNthCalledWith(3, 101);
+    expect(println).toHaveBeenNthCalledWith(4, 201);
+    expect(println).toHaveBeenNthCalledWith(5, 102);
+    expect(println).toHaveBeenNthCalledWith(6, 202);
+  });
+});
