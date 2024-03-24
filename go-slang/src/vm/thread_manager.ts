03f730c04c349ae5c911929ec2f6f8f3c949b958
Sebastian Lee <leehur_sebastian@hotmail.com>
2024-03-24 22:36:43 +0800

    [SL] Remove side effect and return new Context

--- go-slang/src/vm/thread_manager.ts
+++ go-slang/src/vm/thread_manager.ts
@@ -17,22 +17,25 @@ export class ThreadManager {
   /**
    * Updates the scheduler based on the current context.
    */
-  public update_scheduler(curr_context: Context): void {
+  public update_scheduler(curr_context: Context): Context {
     this.thread_instr_count++;
     if (this.thread_instr_count >= INSTRS_PER_THREAD) {
       this.thread_instr_count = 0;
-      this.context_switch(curr_context);
+      return this.context_switch(curr_context);
     }
+    return curr_context;
   }
 
   /**
    * Switches context from the current to the next context.
    */
-  public context_switch(curr_context: Context): void {
+  public context_switch(curr_context: Context): Context {
     if (this.thread_queue.length > 0) {
       this.add_context_to_queue(curr_context);
-      this.restore_vm_context(curr_context);
+      const next_context = this.thread_queue.shift();
+      if (next_context) return next_context;
     }
+    return curr_context
   }
 
   /**
@@ -48,19 +51,5 @@ export class ThreadManager {
         curr_context.sleep_until,
       ),
     );
-  }
-
-  /**
-   * Restores the virtual machine context with the next context
-   * from the thread queue by mutating vm's context object.
-   */
-  private restore_vm_context(curr_context: Context): void {
-    const next_context = this.thread_queue.shift();
-    if (!next_context) return;
-    curr_context.program_counter = next_context.program_counter;
-    curr_context.environment = next_context.environment;
-    curr_context.operand_stack = next_context.operand_stack;
-    curr_context.runtime_stack = next_context.runtime_stack;
-    curr_context.sleep_until = next_context.sleep_until;
   }
 }
