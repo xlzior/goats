333c7379f447c892ad8625a6e4e019e11b15301c
xlzior <wenjun.lye@gmail.com>
2024-03-24 10:11:16 +0800

    [WJ] Revert error message for peek

--- go-slang/src/utils.ts
+++ go-slang/src/utils.ts
@@ -7,6 +7,6 @@ export const is_string = (val: any) => typeof val === "string";
 export const is_boolean = (val: any) => typeof val === "boolean";
 
 export function peek<T>(stack: Array<T>, index: number = 0) {
-  if (stack.length === 0) throw new RuntimeError("Operand stack is empty!");
+  if (stack.length === 0) throw new Error("Stack is empty!");
   return stack[stack.length - 1 - index];
 }
