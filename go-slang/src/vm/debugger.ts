15d35a4ba29e5b818c1726d3245a7acb4079dfb5
Sebastian Lee <leehur_sebastian@hotmail.com>
2024-04-05 14:20:06 +0800

    [SL] Refactor print_stack to make it general

--- go-slang/src/vm/debugger.ts
+++ go-slang/src/vm/debugger.ts
@@ -1,37 +1,29 @@
-import { Memory } from "./memory";
-
 const SPACE = " ";
 const PIPE = "|";
 const DASH = "-";
 const NEWLINE = "\n";
-const STACK_NAME = "operand stack";
 const LEFT_AND_RIGHT_PADDING = 2;
 
 /**
- * Displays the operand stack in a nicely-formatted stack structure
+ * Display and formats stack content into a single-line, well-structured stack representation.
+ *
+ * @param {string} name - The name of the stack.
+ * @param {string[]} content - The content of the stack represented as an array of strings.
+ * @returns {void}
  */
-export function print_os(os: number[], memory: Memory): void {
-  const stack_in_string = os.map((addr) => {
-    const val = memory.address_to_js_value(addr);
-    return val === undefined ? "undefined" : JSON.stringify(val);
-  });
-  const formatted_stack = format_stack(stack_in_string);
-  console.log(formatted_stack);
-}
-
-function format_stack(stack_in_string: string[]): string {
+export function print_stack(name: string, content: string[]): void {
   const stack_width =
     LEFT_AND_RIGHT_PADDING +
-    Math.max(STACK_NAME.length, length_of_longest_string(stack_in_string));
+    Math.max(name.length, length_of_longest_string(content));
   const line_sep = format_line_sep(stack_width);
-  const header = format_data_row(STACK_NAME, stack_width);
+  const header = format_data_row(name, stack_width);
   const result = [line_sep, header, line_sep];
-  for (let i = stack_in_string.length - 1; i >= 0; i--) {
-    result.push(format_data_row(stack_in_string[i], stack_width));
+  for (let i = content.length - 1; i >= 0; i--) {
+    result.push(format_data_row(content[i], stack_width));
     result.push(line_sep);
   }
   const result_to_string = result.join(NEWLINE) + NEWLINE;
-  return result_to_string;
+  console.log(result_to_string);
 }
 
 function format_line_sep(stack_width: number): string {
