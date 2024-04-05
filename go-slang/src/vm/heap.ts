bed3db7447db5081bd2f251b710025dd1f9f488f
xlzior <wenjun.lye@gmail.com>
2024-04-06 00:35:20 +0800

    [WJ] Allocate memory in fixed size blocks

--- go-slang/src/vm/heap.ts
+++ go-slang/src/vm/heap.ts
@@ -1,23 +1,41 @@
+import { RuntimeError } from "../errors";
 import { Tag } from "./tag";
 
 const WORD_SIZE = 8;
 const SIZE_OFFSET = 5;
-// const NODE_SIZE = 10; // js-slang build failed due to unused var
+export const NODE_SIZE = 10; // js-slang build failed due to unused var
 
 export class Heap {
   free: number = 0;
   heap_size: number;
   view: DataView;
+  get_roots: () => number[];
 
-  constructor(heap_size: number) {
+  constructor(heap_size: number, get_roots: () => number[]) {
     this.heap_size = heap_size;
+    this.get_roots = get_roots;
     const data = new ArrayBuffer(heap_size * WORD_SIZE);
     this.view = new DataView(data);
+
+    // initialise free list to be a chain of all nodes
+    let i = 0;
+    for (i = 0; i <= heap_size - NODE_SIZE; i += NODE_SIZE) {
+      this.set(i, i + NODE_SIZE);
+    }
+    this.set(i - NODE_SIZE, -1);
   }
 
   allocate(tag: Tag, size: number): number {
+    if (size > NODE_SIZE)
+      throw new RuntimeError("Node size cannot be larger than 10 words");
+
+    if (this.free === -1) {
+      console.log(this.get_roots());
+      throw new RuntimeError("Heap is full");
+    }
+
     const address = this.free;
-    this.free += size;
+    this.free = this.get(this.free);
     this.view.setUint8(address * WORD_SIZE, tag);
     this.view.setUint16(address * WORD_SIZE + SIZE_OFFSET, size);
     return address;
