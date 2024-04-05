382951a0f7488b7ab383b067d782c2c3b481dec3
xlzior <wenjun.lye@gmail.com>
2024-04-05 11:50:47 +0800

    [WJ] Change helper.ts to take in a .go file

---
+++ go-slang/examples/run.ts
@@ -0,0 +1,31 @@
+// can be run using
+// npx ts-node run.ts < xxx.go
+
+import { GolangRunner } from "../src";
+import { BuiltinFunction } from "../src/types";
+
+const external_builtins: Record<string, BuiltinFunction> = {
+  Println: {
+    arity: 1,
+    apply: (v: any) => console.log(v),
+  },
+};
+const runner = new GolangRunner(external_builtins);
+
+process.stdin.setEncoding("utf8");
+let program = "";
+process.stdin.on("data", (chunk) => {
+  program += chunk;
+});
+
+process.stdin.on("end", () => {
+  console.log("Running Go program:");
+  runner
+    .execute(program)
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
