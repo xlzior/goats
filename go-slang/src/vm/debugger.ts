df432312b281dec41f2abafcdc0806941af37448
Sebastian Lee <leehur_sebastian@hotmail.com>
2024-04-05 11:14:12 +0800

    [SL] Refactor print_os slightly

--- go-slang/src/vm/debugger.ts
+++ go-slang/src/vm/debugger.ts
@@ -1,3 +1,5 @@
+import { Memory } from "./memory";
+
 const SPACE = " ";
 const PIPE = "|";
 const DASH = "-";
@@ -5,19 +7,31 @@ const NEWLINE = "\n";
 const STACK_NAME = "operand stack";
 const LEFT_AND_RIGHT_PADDING = 2;
 
-export function print_os(stack_in_string: string[]): void {
+/**
+ * Displays the operand stack in a nicely-formatted stack structure
+ */
+export function print_os(os: number[], memory: Memory): void {
+  const stack_in_string = os.map((addr) => {
+    const val = memory.address_to_js_value(addr);
+    return val === undefined ? "undefined" : JSON.stringify(val);
+  });
+  const formatted_stack = format_stack(stack_in_string);
+  console.log(formatted_stack);
+}
+
+function format_stack(stack_in_string: string[]): string {
   const stack_width =
     LEFT_AND_RIGHT_PADDING +
     Math.max(STACK_NAME.length, length_of_longest_string(stack_in_string));
   const line_sep = format_line_sep(stack_width);
   const header = format_data_row(STACK_NAME, stack_width);
   const result = [line_sep, header, line_sep];
-  stack_in_string.reverse().forEach((val) => {
-    result.push(format_data_row(val, stack_width));
+  for (let i = stack_in_string.length - 1; i >= 0; i--) {
+    result.push(format_data_row(stack_in_string[i], stack_width));
     result.push(line_sep);
-  });
+  }
   const result_to_string = result.join(NEWLINE) + NEWLINE;
-  console.log(result_to_string);
+  return result_to_string;
 }
 
 function format_line_sep(stack_width: number): string {
