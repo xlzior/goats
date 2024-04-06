79b0e6a865d0e32a239fd553007d116108d606d3
Sebastian Lee <leehur_sebastian@hotmail.com>
2024-04-06 09:45:12 +0800

    [SL] Update hof example

--- go-slang/examples/hof.go
+++ go-slang/examples/hof.go
@@ -4,12 +4,20 @@ func add(a int, b int) int {
   return a + b
 }
 
-func applyFunc(f func(int, int) int) int {
-  x := f(3, 5)
+func proxy() func(int, int) int {
+	x := "all i do is to return another function"
+	return add
+}
+
+func applyFunc(x, y int, f func(int, int) int) int {
+  x := f(x, y)
   return x
 }
 
 func main() {
-  result := applyFunc(add)
-  Println(result)
+  res1 := applyFunc(3, 5, add) // receiving function as arg
+	fn := proxy() // returning function
+  res2 := fn(10, 12)
+	Println(res1)
+	Println(res2)
 }
