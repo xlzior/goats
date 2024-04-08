78c035bde3f1a8886692883e53aa2b8d5cd3662d
xlzior <wenjun.lye@gmail.com>
2024-04-08 15:40:58 +0800

    [WJ] Fix typo in default memory size

--- go-slang/src/vm/config.ts
+++ go-slang/src/vm/config.ts
@@ -1,12 +1,12 @@
 export class Config {
   constructor(
-    public readonly memory: number = 1000000,
+    public readonly memory: number = 10_000_000,
     public readonly debug_os: boolean = false,
   ) {}
 
   static from_args(args: Record<string, string>): Config {
     return new Config(
-      parseInt(args.memory) || 10000000,
+      parseInt(args.memory) || 10_000_000,
       args.debug_os === "true",
     );
   }
